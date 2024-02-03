9d169744d9dc1a2231e786ac32af9dd633865b5e
Ahmed Tharwat <ahmedlordexg@gmail.com>
2024-02-03 03:59:23 +0200

    semiFinals

--- app.c
+++ app.c
@@ -1,115 +1,226 @@
-#include<stdio.h>  
+#include <stdio.h>
 #include <stdlib.h>
-#include<conio.h> 
+#include <conio.h>
 #include <string.h>
-// skip welcome
+//  welcome
+#define CYAN printf("\033[0;36m");
+#define RESET printf("\x1b[0m");
+#define RED printf("\x1b[31m"),
 
-char SQUARES_NUMBERS[3][3] = {{'1','2','3'},{'4','5','6'},{'7','8','9'}};
+int counter (char* message) {
+    int count;
+    for (count = 0; message[count] != '\0'; count++); 
+    return count;
+}
 
-void PRINT_TABLE(){
-    printf("-------------\n");
-    for (int i = 0; i < 3; i++)
-    {
-        printf("|");
-        
-        for (int j = 0; j <= 2; j++)
-            printf(" %c |", SQUARES_NUMBERS[i][j]);
-        
-        printf("\n");
-        printf("-------------\n");
-    }    
+void welcomemessage (char* message) {
+    int n = counter(message) ;  
+    for (int i = 7; i < n; i++) printf("-");
+    printf("\n| %s |\n", message);
+    for (int i = 7; i < n; i++) printf("-");
+    printf("\n");
+}
+
+void GameRules() {
+    RED
+    printf("\x1b[31m""Rules of the game:\n""\x1b[31m");
+    RESET;
+    printf("\n");
+    printf("1- Get three of your marks (X or O) in a row either horizontally, vertically, or diagonally.\n");
+    printf("\n");
+    printf("2- Players take turns placing their mark in empty squares on a 3x3 grid.\n");
+    printf("\n");
+    printf("3- The first player to align three marks in a row wins the game.\n");
+    printf("\n");
+    printf("4- If all squares are filled and no player has three marks in a row, the game is declared a draw.\n");
+    printf("\n");
+    printf("----------------------------------------\n");
+    printf("----------------------------------------\n");
+    // printf("\033[0m");
 }
 
 
 
 
+/*-------------------------------------------*/
 
 
+
+char SQUARES_NUMBERS[3][3] = {{'1','2','3'},{'4','5','6'},{'7','8','9'}};
+
+/*-------------------------------------------*/
+int playerX = 0;
+int playerO = 0;
+int end_game_counter = 0;
+void PRINT_TABLE(){
+    printf("-------------\n");
+    for (int i = 0; i < 3; i++)
+    {
+        printf("|");
+
+        for (int j = 0; j <= 2; j++)
+            printf(" %c |", SQUARES_NUMBERS[i][j]);
+
+        printf("\n");
+        printf("-------------\n");
+    }
+    
+    if(playerX) printf("Player X won\n");
+    else if (playerO) printf("Player O won\n\n");
+}
+
+// Varibals
 int X_USER;
 int O_USER;
 int flag = 0;
 int inputs_counter = 0;
 
+/*----------------------------------------*/
+
 int CHECK_WINNER(){
-    for(int i=0; i<3; i++)
-        if(SQUARES_NUMBERS[i][0] == SQUARES_NUMBERS[i][1] && SQUARES_NUMBERS[i][0] == SQUARES_NUMBERS[i][2] || SQUARES_NUMBERS[0][i] == SQUARES_NUMBERS[1][i] && SQUARES_NUMBERS[0][i] == SQUARES_NUMBERS[2][i])
-        {
-            //BUGGGGGGGGGGGGGGGGGGGGG
-                printf("PLAYER %c WON!!\n", SQUARES_NUMBERS[i][0]);
-            return 1;
-        }
+    //Check Win for rows
+    if(inputs_counter != 9){
+        for(int i=0; i<3; i++)
+            if(SQUARES_NUMBERS[i][0] == SQUARES_NUMBERS[i][1] && SQUARES_NUMBERS[i][0] == SQUARES_NUMBERS[i][2])/*|| SQUARES_NUMBERS[0][i] == SQUARES_NUMBERS[1][i] && SQUARES_NUMBERS[0][i] == SQUARES_NUMBERS[2][i])*/
+                if(SQUARES_NUMBERS[i][0] == 'O')
+                    playerO++;
+                else
+                    playerX++;
+        
+    // Check win for coloumns
+    for(int i = 0; i < 3; i++)
+        if(SQUARES_NUMBERS[0][i] == SQUARES_NUMBERS[1][i] && SQUARES_NUMBERS[0][i] == SQUARES_NUMBERS[2][i])
+            if(SQUARES_NUMBERS[0][i]=='O')
+                playerO++;
+            else
+                playerX++;
 
     //checking the win for both diagonal
 
-    if(SQUARES_NUMBERS[0][0] == SQUARES_NUMBERS[1][1] && SQUARES_NUMBERS[0][0] == SQUARES_NUMBERS[2][2] || SQUARES_NUMBERS[0][2] == SQUARES_NUMBERS[1][1] && SQUARES_NUMBERS[0][2] == SQUARES_NUMBERS[2][0])
-    {
-        //BUGGGGGGGGGGGGGGGGGGGGG
-        if(inputs_counter%2)
-                printf("PLAYER 'X' WON!!\n");
-            else
-                printf("PLAYER 'O' WON!!\n");
+    if(SQUARES_NUMBERS[0][0] == SQUARES_NUMBERS[1][1]  && SQUARES_NUMBERS[0][0] == SQUARES_NUMBERS[2][2])
+        if(SQUARES_NUMBERS[0][0]=='O')
+            playerO++;
+        else
+            playerX++;
+
+
+    if(SQUARES_NUMBERS[0][2] == SQUARES_NUMBERS[1][1] && SQUARES_NUMBERS[0][2] == SQUARES_NUMBERS[2][0])
+        if(SQUARES_NUMBERS[0][2]=='O')
+            playerO++;
+        else
+            playerX++;
+    
+    
+}
+
+// Check for a tie
+
+// working but prints it is a tie 2 times
+
+    else if(end_game_counter == 9 ){
+        printf("It is a Tie!!\n");
         return 1;
     }
+
     // 0
     // 1 --> X
     // 2 --> O
     // 3 --> X
     return 0;
-};
+}
+
+/*----------------------------*/
 
 void UPDATE(int USER){
-    if(flag){
-        if(USER < 4)
-            SQUARES_NUMBERS[0][(USER-1)%3] = 'X';
-        else if(USER < 7)
-            SQUARES_NUMBERS[1][(USER-1)%3] = 'X';
-        else if(USER < 10) 
-            SQUARES_NUMBERS[2][(USER-1)%3] = 'X';
-    }else{
-        if(USER < 4)
-            SQUARES_NUMBERS[0][(USER-1)%3] = 'O';
-        else if(USER < 7)
-            SQUARES_NUMBERS[1][(USER-1)%3] = 'O';
-        else if(USER < 10) 
-            SQUARES_NUMBERS[2][(USER-1)%3] = 'O';
+    // invalid number
+    if(USER > 9 || USER < 1){
+        printf("Enter a valid number\n");
+        end_game_counter--;
     }
-    PRINT_TABLE();
+    
+    // if already taken
+    if( SQUARES_NUMBERS[(USER-1)/3][(USER-1)%3] == 'X' || 
+        SQUARES_NUMBERS[(USER-1)/3][(USER-1)%3] == 'O'){
+        printf("\nAlready taken\n");
+        printf("Enter another number:\n");
+        
+        // because it is considered as a round
+        //but we don't want that
+        end_game_counter--; 
+        
+        // when input the same number twice
+        // The third symbol will be the first symbol
+        // input [1] -> X
+        // input [1] -> for "O" but 1 is already taken
+        // input [2] -> for "O" but we get X again in 2
+        // that's because the change of the flag every input
+        // this condition prevent that bug
+        if (flag) flag = 0;
+        else    flag = 1;
+        
+        return;
+    }
+    
+    if(flag)
+        SQUARES_NUMBERS[(USER-1)/3][(USER-1)%3] = 'X';
+    else
+        SQUARES_NUMBERS[(USER-1)/3][(USER-1)%3] = 'O';
 };
+/*------------------------------------------*/
 
 void USER_INPUT_AND_UPDATES(){
     // take input from user
     // update global array
-    // input validation system
-    
+    // input validation system    
         if(!flag){
         scanf("%d", &X_USER, inputs_counter++);
         flag = 1;
         UPDATE(X_USER);
         if(inputs_counter > 4) // no one will win before 5 inputs
             CHECK_WINNER();
-        
+
     }else if(flag){
         scanf("%d", &O_USER, inputs_counter++);
         flag = 0;
         UPDATE(O_USER);
         if(inputs_counter > 4)
             CHECK_WINNER();
-    }
+    } 
 }
-
+/*--------------------------------------*/
 //logic
 // global scope
-    // 3x3 array from 1 to 9 
+    // 3x3 array from 1 to 9
 
-// function to print the table  
+// function to print the table
 
-    
-int end_game_counter = 0;
-int main()  
-{  
-    printf("Welcome to JavaTpoint");  
-    printf("\nThis is the Second sentence of the program");  
-    clrscr();  
-    printf("Output After using clrscr() function");  
-    return 0;  
-}  
+
+
+/*int main()
+{
+    printf("Welcome to JavaTpoint");
+    printf("\nThis is the Second sentence of the program");
+    clrscr();
+    printf("Output After using clrscr() function");
+    return 0;
+}*/
+
+
+int main(){
+   welcomemessage ("\033[1;34mwelcome to tic tac toe\033[0m");
+    GameRules();
+
+    int USER;
+    while(end_game_counter != 9){
+        PRINT_TABLE();
+        USER_INPUT_AND_UPDATES();
+        end_game_counter++;
+        if(playerO ^ playerX){
+            // newgame/end function
+            
+            PRINT_TABLE(); // final result 
+            printf("\nNEW OR END");
+            break;
+        }
+    }
+}
